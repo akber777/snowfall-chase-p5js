28bec9a266fe8b7ca3bfd96004df5daaf57e857e
akber <akberakhmedzadeh@gmail.com>
2024-07-31 22:15:07 +0400

    all bugs fixed

--- app/app.js
+++ app/app.js
@@ -1,11 +1,19 @@
 "use strict";
 /// <reference path="../node_modules/@types/p5/global.d.ts" />
-class SNOWFALL_CHASE {
-    constructor() {
+var SNOWFALL_CHASE = /** @class */ (function () {
+    function SNOWFALL_CHASE() {
         this.width = windowWidth;
         this.height = windowHeight;
         this.ellipseArr = [];
         this.bgImage = null;
+        this.hitsItemArr = [];
+        this.hitsItemCount = 0;
+        this.hitsItemLimit = 680;
+        this.hitsItemSize = 20;
+        this.hitsItemSpacing = 5;
+        this.snowBallMoving = false;
+        this.crashed = false;
+        this.gameOver = false;
         this.boardOptions = {
             left: floor((this.width - 700) / 2),
             top: floor((this.height - 700) / 2),
@@ -17,27 +25,30 @@ class SNOWFALL_CHASE {
             hitLeft: floor((this.width - 700) / 2) + 250,
             hitTop: floor((this.height - 700) / 2) + 650,
             hitBoardWidth: 200,
-            hitBoardHeight: 30,
+            hitBoardHeight: 20,
             hitBorderRadius: 0,
             speed: 10
         };
-        this.hitsItemArr = [];
-        this.hitsItemCount = 0;
-        this.hitsItemLimit = 680;
-        this.hitsItemSize = 20;
-        this.hitsItemSpacing = 5;
+        this.snowBallOptions = {
+            x: this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth / 2,
+            y: this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight / 2 - this.hitBoardOptions.hitBoardHeight - this.hitsItemSpacing,
+            snowBallDiameter: 20,
+            speedX: 2,
+            speedY: 8
+        };
     }
-    preloadBackground() {
+    SNOWFALL_CHASE.prototype.preloadBackground = function () {
         this.bgImage = loadImage('https://i.redd.it/y4iy0n11q25a1.jpg');
-    }
-    drawBackground() {
+    };
+    SNOWFALL_CHASE.prototype.drawBackground = function () {
         image(this.bgImage, 0, 0, windowWidth - 18, windowHeight);
-    }
-    createCanvas() {
-        createCanvas(windowWidth - 18, windowHeight);
-    }
-    snowEffect() {
-        const snowData = {
+    };
+    SNOWFALL_CHASE.prototype.createCanvas = function () {
+        createCanvas(windowWidth - 18, windowHeight - 10);
+    };
+    SNOWFALL_CHASE.prototype.snowEffect = function () {
+        var _this = this;
+        var snowData = {
             width: 5,
             height: 5,
             left: floor(random(0, this.width)),
@@ -45,8 +56,9 @@ class SNOWFALL_CHASE {
             speed: 1 + random(2)
         };
         this.ellipseArr.push(snowData);
-        let tempArr = [];
-        for (const elem of this.ellipseArr) {
+        var tempArr = [];
+        for (var _i = 0, _a = this.ellipseArr; _i < _a.length; _i++) {
+            var elem = _a[_i];
             elem.top += floor(elem.speed);
             elem.left += floor(random(0, 1));
             if (elem.top > this.boardOptions.top && elem.top < this.boardOptions.top + this.boardOptions.boardHeight &&
@@ -61,16 +73,16 @@ class SNOWFALL_CHASE {
             noStroke();
         }
         this.ellipseArr = tempArr;
-        this.ellipseArr = this.ellipseArr.filter(elem => elem.top < this.height);
-    }
-    createBoard() {
+        this.ellipseArr = this.ellipseArr.filter(function (elem) { return elem.top < _this.height; });
+    };
+    SNOWFALL_CHASE.prototype.createBoard = function () {
         push();
         fill(0);
         rect(this.boardOptions.left, this.boardOptions.top, this.boardOptions.boardWidth, this.boardOptions.boardHeight, this.boardOptions.borderRadius);
         pop();
         this.createHitBoard();
-    }
-    createHitBoard() {
+    };
+    SNOWFALL_CHASE.prototype.createHitBoard = function () {
         fill(255);
         rect(this.hitBoardOptions.hitLeft, this.hitBoardOptions.hitTop, this.hitBoardOptions.hitBoardWidth, this.hitBoardOptions.hitBoardHeight, this.hitBoardOptions.hitBorderRadius);
         if (keyIsDown(LEFT_ARROW)) {
@@ -85,26 +97,109 @@ class SNOWFALL_CHASE {
         if (this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth > this.boardOptions.left + this.boardOptions.boardWidth) {
             this.hitBoardOptions.hitLeft = this.boardOptions.left + this.boardOptions.boardWidth - this.hitBoardOptions.hitBoardWidth;
         }
-    }
-    createHitsItem() {
-        const cols = Math.floor(this.boardOptions.boardWidth / (this.hitsItemSize + this.hitsItemSpacing));
-        const remainingSpace = this.boardOptions.boardWidth - (cols * this.hitsItemSize + (cols - 1) * this.hitsItemSpacing);
-        const adjustedSpacing = this.hitsItemSpacing + remainingSpace / (cols - 1);
-        const rows = 3;
-        if (this.hitsItemArr.length < cols * rows) {
-            for (let i = 0; i < cols * rows; i++) {
-                const x = this.boardOptions.left + (i % cols) * (this.hitsItemSize + adjustedSpacing);
-                const y = this.boardOptions.top + Math.floor(i / cols) * (this.hitsItemSize + adjustedSpacing);
-                this.hitsItemArr.push({ x, y });
+    };
+    SNOWFALL_CHASE.prototype.createHitsItem = function () {
+        if (this.hitsItemArr.length === 0) {
+            var cols_1 = Math.floor(this.boardOptions.boardWidth / (this.hitsItemSize + this.hitsItemSpacing));
+            var remainingSpace = this.boardOptions.boardWidth - (cols_1 * this.hitsItemSize + (cols_1 - 1) * this.hitsItemSpacing);
+            var adjustedSpacing = this.hitsItemSpacing + remainingSpace / (cols_1 - 1);
+            var rows_1 = 5;
+            var patterns = [
+                function (col, row) { return true; },
+                function (col, row) { return row % 2 === 0; },
+                function (col, row) { return col === 0 || col === cols_1 - 1; },
+                function (col, row) { return (col + row) % 2 === 0; },
+                function (col, row) { return col === row; },
+                function (col, row) { return col >= row && col < cols_1 - row; },
+                function (col, row) { return row === 0 || row === rows_1 - 1 || col === 0 || col === cols_1 - 1; },
+                function (col, row) { return col === row || col === cols_1 - row - 1; },
+                function (col, row) { return Math.random() > 0.5; },
+                function (col, row) { return col >= row && col % 2 === 0; },
+                function (col, row) { return (row === 0 || col === 0) || (col === cols_1 - 1 || row === rows_1 - 1); },
+                function (col, row) { return col % 3 === 0 && row % 3 === 0; },
+                function (col, row) { return col % 2 === 0 && row % 3 === 0; },
+                function (col, row) { return (col + row) % 3 === 0; },
+                function (col, row) { return col > row && col < cols_1 - row - 1; },
+                function (col, row) { return row < rows_1 / 2; },
+                function (col, row) { return col > cols_1 / 2; },
+                function (col, row) { return row === col || row === cols_1 - col - 1; },
+                function (col, row) { return col % 2 === 1 && row % 2 === 1; },
+                function (col, row) { return col > Math.floor(cols_1 / 4) && col < Math.floor(3 * cols_1 / 4) && row > Math.floor(rows_1 / 4) && row < Math.floor(3 * rows_1 / 4); }
+            ];
+            var pattern = random(patterns);
+            for (var row = 0; row < rows_1; row++) {
+                for (var col = 0; col < cols_1; col++) {
+                    if (pattern(col, row)) {
+                        var x = this.boardOptions.left + col * (this.hitsItemSize + adjustedSpacing);
+                        var y = this.boardOptions.top + row * (this.hitsItemSize + adjustedSpacing);
+                        this.hitsItemArr.push({ x: x, y: y });
+                    }
+                }
             }
         }
-        for (const item of this.hitsItemArr) {
+        for (var _i = 0, _a = this.hitsItemArr; _i < _a.length; _i++) {
+            var item = _a[_i];
             fill(255);
             rect(item.x, item.y, this.hitsItemSize, this.hitsItemSize);
         }
-    }
-}
-let snowFallChase;
+        this.createSnowBall();
+    };
+    SNOWFALL_CHASE.prototype.createSnowBall = function () {
+        if (this.gameOver) {
+            return;
+        }
+        fill('#fff');
+        ellipse(this.snowBallOptions.x, this.snowBallOptions.y, this.snowBallOptions.snowBallDiameter, this.snowBallOptions.snowBallDiameter);
+        if (this.snowBallMoving) {
+            this.snowBallOptions.y += this.snowBallOptions.speedY;
+            this.snowBallOptions.x += this.snowBallOptions.speedX;
+            if (this.snowBallOptions.y + this.snowBallOptions.snowBallDiameter / 2 >= this.hitBoardOptions.hitTop &&
+                this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 <= this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight &&
+                this.snowBallOptions.x + this.snowBallOptions.snowBallDiameter / 2 >= this.hitBoardOptions.hitLeft &&
+                this.snowBallOptions.x - this.snowBallOptions.snowBallDiameter / 2 <= this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth) {
+                this.snowBallOptions.y = this.hitBoardOptions.hitTop - this.snowBallOptions.snowBallDiameter / 2;
+                this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+            }
+            for (var index = 0; index < this.hitsItemArr.length; index++) {
+                var element = this.hitsItemArr[index];
+                var distance = dist(this.snowBallOptions.x, this.snowBallOptions.y, element.x + this.hitsItemSize / 2, element.y + this.hitsItemSize / 2);
+                if (distance < this.snowBallOptions.snowBallDiameter / 2 + this.hitsItemSize / 2) {
+                    this.hitsItemArr.splice(index, 1);
+                    this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+                    break;
+                }
+            }
+            // left top right border of boardoptions
+            if (this.snowBallOptions.x - this.snowBallOptions.snowBallDiameter / 2 <= this.boardOptions.left ||
+                this.snowBallOptions.x + this.snowBallOptions.snowBallDiameter / 2 >= this.boardOptions.left + this.boardOptions.boardWidth) {
+                this.snowBallOptions.speedX = -this.snowBallOptions.speedX;
+            }
+            if (this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 <= this.boardOptions.top) {
+                this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+            }
+            // Game Over 
+            if (this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 > this.height) {
+                this.gameOver = true;
+                this.snowBallMoving = false;
+            }
+        }
+    };
+    SNOWFALL_CHASE.prototype.resetSnowBall = function () {
+        this.snowBallOptions.x = this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth / 2;
+        this.snowBallOptions.y = this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight / 2 - this.hitBoardOptions.hitBoardHeight - this.hitsItemSpacing;
+        this.snowBallOptions.speedY = abs(this.snowBallOptions.speedY);
+        this.snowBallOptions.speedX = random(-2, 2);
+        this.crashed = false;
+        this.gameOver = false;
+    };
+    SNOWFALL_CHASE.prototype.handleKeyPressed = function () {
+        if (keyCode === 32) {
+            this.snowBallMoving = true;
+        }
+    };
+    return SNOWFALL_CHASE;
+}());
+var snowFallChase;
 function preload() {
     snowFallChase = new SNOWFALL_CHASE();
     snowFallChase.preloadBackground();
@@ -113,8 +208,21 @@ function setup() {
     snowFallChase.createCanvas();
 }
 function draw() {
-    snowFallChase.drawBackground();
-    snowFallChase.createBoard();
-    snowFallChase.snowEffect();
-    snowFallChase.createHitsItem();
+    if (snowFallChase.gameOver) {
+        snowFallChase.drawBackground();
+        snowFallChase.createBoard();
+        fill(255);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Game Over", snowFallChase.boardOptions.left + snowFallChase.boardOptions.boardWidth / 2, snowFallChase.boardOptions.top + snowFallChase.boardOptions.boardHeight / 2);
+    }
+    else {
+        snowFallChase.drawBackground();
+        snowFallChase.snowEffect();
+        snowFallChase.createBoard();
+        snowFallChase.createHitsItem();
+    }
+}
+function keyPressed() {
+    snowFallChase.handleKeyPressed();
 }
--- app/app.ts
+++ app/app.ts
@@ -5,6 +5,14 @@ class SNOWFALL_CHASE {
     height = windowHeight;
     protected ellipseArr: any[] = [];
     bgImage: any = null;
+    hitsItemArr: any[] = [];
+    hitsItemCount = 0;
+    hitsItemLimit = 680;
+    hitsItemSize = 20;
+    hitsItemSpacing = 5;
+    snowBallMoving = false;
+    crashed = false;
+    gameOver = false;
     boardOptions = {
         left: floor((this.width - 700) / 2),
         top: floor((this.height - 700) / 2),
@@ -16,15 +24,17 @@ class SNOWFALL_CHASE {
         hitLeft: floor((this.width - 700) / 2) + 250,
         hitTop: floor((this.height - 700) / 2) + 650,
         hitBoardWidth: 200,
-        hitBoardHeight: 30,
+        hitBoardHeight: 20,
         hitBorderRadius: 0,
         speed: 10
     };
-    hitsItemArr: any[] = [];
-    hitsItemCount = 0;
-    hitsItemLimit = 680;
-    hitsItemSize = 20;
-    hitsItemSpacing = 5;
+    snowBallOptions = {
+        x: this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth / 2,
+        y: this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight / 2 - this.hitBoardOptions.hitBoardHeight - this.hitsItemSpacing,
+        snowBallDiameter: 20,
+        speedX: 2,
+        speedY: 8
+    };
 
     preloadBackground() {
         this.bgImage = loadImage('https://i.redd.it/y4iy0n11q25a1.jpg');
@@ -35,7 +45,7 @@ class SNOWFALL_CHASE {
     }
 
     createCanvas() {
-        createCanvas(windowWidth - 18, windowHeight);
+        createCanvas(windowWidth - 18, windowHeight - 10);
     }
 
     snowEffect() {
@@ -98,22 +108,118 @@ class SNOWFALL_CHASE {
     }
 
     createHitsItem() {
-        const cols = Math.floor(this.boardOptions.boardWidth / (this.hitsItemSize + this.hitsItemSpacing));
-        const remainingSpace = this.boardOptions.boardWidth - (cols * this.hitsItemSize + (cols - 1) * this.hitsItemSpacing);
-        const adjustedSpacing = this.hitsItemSpacing + remainingSpace / (cols - 1);
-        const rows = 3;
+        if (this.hitsItemArr.length === 0) {
+            const cols = Math.floor(this.boardOptions.boardWidth / (this.hitsItemSize + this.hitsItemSpacing));
+            const remainingSpace = this.boardOptions.boardWidth - (cols * this.hitsItemSize + (cols - 1) * this.hitsItemSpacing);
+            const adjustedSpacing = this.hitsItemSpacing + remainingSpace / (cols - 1);
+            const rows = 5;
 
-        if (this.hitsItemArr.length < cols * rows) {
-            for (let i = 0; i < cols * rows; i++) {
-                const x = this.boardOptions.left + (i % cols) * (this.hitsItemSize + adjustedSpacing);
-                const y = this.boardOptions.top + Math.floor(i / cols) * (this.hitsItemSize + adjustedSpacing);
-                this.hitsItemArr.push({ x, y });
+            const patterns: ((col: number, row: number) => boolean)[] = [
+                (col: number, row: number) => true,
+                (col: number, row: number) => row % 2 === 0,
+                (col: number, row: number) => col === 0 || col === cols - 1,
+                (col: number, row: number) => (col + row) % 2 === 0,
+                (col: number, row: number) => col === row,
+                (col: number, row: number) => col >= row && col < cols - row,
+                (col: number, row: number) => row === 0 || row === rows - 1 || col === 0 || col === cols - 1,
+                (col: number, row: number) => col === row || col === cols - row - 1,
+                (col: number, row: number) => Math.random() > 0.5,
+                (col: number, row: number) => col >= row && col % 2 === 0,
+                (col: number, row: number) => (row === 0 || col === 0) || (col === cols - 1 || row === rows - 1),
+                (col: number, row: number) => col % 3 === 0 && row % 3 === 0,
+                (col: number, row: number) => col % 2 === 0 && row % 3 === 0,
+                (col: number, row: number) => (col + row) % 3 === 0,
+                (col: number, row: number) => col > row && col < cols - row - 1,
+                (col: number, row: number) => row < rows / 2,
+                (col: number, row: number) => col > cols / 2,
+                (col: number, row: number) => row === col || row === cols - col - 1,
+                (col: number, row: number) => col % 2 === 1 && row % 2 === 1,
+                (col: number, row: number) => col > Math.floor(cols / 4) && col < Math.floor(3 * cols / 4) && row > Math.floor(rows / 4) && row < Math.floor(3 * rows / 4)
+            ];
+
+            const pattern = random(patterns);
+
+            for (let row = 0; row < rows; row++) {
+                for (let col = 0; col < cols; col++) {
+                    if (pattern(col, row)) {
+                        const x = this.boardOptions.left + col * (this.hitsItemSize + adjustedSpacing);
+                        const y = this.boardOptions.top + row * (this.hitsItemSize + adjustedSpacing);
+                        this.hitsItemArr.push({ x, y });
+                    }
+                }
             }
         }
 
         for (const item of this.hitsItemArr) {
             fill(255);
             rect(item.x, item.y, this.hitsItemSize, this.hitsItemSize);
+        }
+
+        this.createSnowBall();
+    }
+
+    createSnowBall() {
+        if (this.gameOver) {
+            return;
+        }
+
+        fill('#fff');
+        ellipse(this.snowBallOptions.x, this.snowBallOptions.y, this.snowBallOptions.snowBallDiameter, this.snowBallOptions.snowBallDiameter);
+
+        if (this.snowBallMoving) {
+            this.snowBallOptions.y += this.snowBallOptions.speedY;
+            this.snowBallOptions.x += this.snowBallOptions.speedX;
+
+            if (
+                this.snowBallOptions.y + this.snowBallOptions.snowBallDiameter / 2 >= this.hitBoardOptions.hitTop &&
+                this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 <= this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight &&
+                this.snowBallOptions.x + this.snowBallOptions.snowBallDiameter / 2 >= this.hitBoardOptions.hitLeft &&
+                this.snowBallOptions.x - this.snowBallOptions.snowBallDiameter / 2 <= this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth
+            ) {
+                this.snowBallOptions.y = this.hitBoardOptions.hitTop - this.snowBallOptions.snowBallDiameter / 2;
+                this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+            }
+
+
+            for (let index = 0; index < this.hitsItemArr.length; index++) {
+                const element = this.hitsItemArr[index];
+                const distance = dist(this.snowBallOptions.x, this.snowBallOptions.y, element.x + this.hitsItemSize / 2, element.y + this.hitsItemSize / 2);
+                if (distance < this.snowBallOptions.snowBallDiameter / 2 + this.hitsItemSize / 2) {
+                    this.hitsItemArr.splice(index, 1);
+                    this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+                    break;
+                }
+            }
+
+            // left top right border of boardoptions
+            if (this.snowBallOptions.x - this.snowBallOptions.snowBallDiameter / 2 <= this.boardOptions.left ||
+                this.snowBallOptions.x + this.snowBallOptions.snowBallDiameter / 2 >= this.boardOptions.left + this.boardOptions.boardWidth) {
+                this.snowBallOptions.speedX = -this.snowBallOptions.speedX;
+            }
+            if (this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 <= this.boardOptions.top) {
+                this.snowBallOptions.speedY = -this.snowBallOptions.speedY;
+            }
+
+            // Game Over 
+            if (this.snowBallOptions.y - this.snowBallOptions.snowBallDiameter / 2 > this.height) {
+                this.gameOver = true;
+                this.snowBallMoving = false;
+            }
+        }
+    }
+
+    resetSnowBall() {
+        this.snowBallOptions.x = this.hitBoardOptions.hitLeft + this.hitBoardOptions.hitBoardWidth / 2;
+        this.snowBallOptions.y = this.hitBoardOptions.hitTop + this.hitBoardOptions.hitBoardHeight / 2 - this.hitBoardOptions.hitBoardHeight - this.hitsItemSpacing;
+        this.snowBallOptions.speedY = abs(this.snowBallOptions.speedY);
+        this.snowBallOptions.speedX = random(-2, 2);
+        this.crashed = false;
+        this.gameOver = false;
+    }
+
+    handleKeyPressed() {
+        if (keyCode === 32) {
+            this.snowBallMoving = true;
         }
     }
 }
@@ -130,8 +236,21 @@ function setup() {
 }
 
 function draw() {
-    snowFallChase.drawBackground();
-    snowFallChase.createBoard();
-    snowFallChase.snowEffect();
-    snowFallChase.createHitsItem();
+    if (snowFallChase.gameOver) {
+        snowFallChase.drawBackground();
+        snowFallChase.createBoard();
+        fill(255);
+        textSize(32);
+        textAlign(CENTER, CENTER);
+        text("Game Over", snowFallChase.boardOptions.left + snowFallChase.boardOptions.boardWidth / 2, snowFallChase.boardOptions.top + snowFallChase.boardOptions.boardHeight / 2);
+    } else {
+        snowFallChase.drawBackground();
+        snowFallChase.snowEffect();
+        snowFallChase.createBoard();
+        snowFallChase.createHitsItem();
+    }
+}
+
+function keyPressed() {
+    snowFallChase.handleKeyPressed();
 }
